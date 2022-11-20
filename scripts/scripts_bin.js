aba4d8813a734f12160601e0cc1d06aa393bda3e
SamSamsung <76557309+SamSamsung@users.noreply.github.com>
2022-11-20 01:05:37 +0100

    Update scripts_bin.js

--- scripts/scripts_bin.js
+++ scripts/scripts_bin.js
@@ -1,4 +1,3 @@
-
 function rand(){
     return Math.floor(Math.random() * 255)
 }
@@ -7,9 +6,6 @@ setTimeout(function(){
     var ran = rand()
     document.getElementById("Nombre a trouver").innerHTML = ran
 }, 10)
-
-
-
 
 
 function changer_nombre(id){
@@ -24,17 +20,39 @@ function changer_nombre(id){
 
 }
 
+function conv(chiffre){
+    L = []
+    q = 1
+    while(q != 0){
+        q = Math.floor(chiffre/2);
+        bits = chiffre % 2;
+        chiffre = q;
+        L.push(bits);
+    }
+    L.reverse()
+    return L
+}
+
+
 function verif(){
     let cpt = 0
+    nb = ""
     for(i=0; i<=7;i++){
         
         if(document.getElementById(i).innerHTML === "1"){
             cpt = cpt + 2**i
-        } 
+            nb = nb + "1"
+        } else{
+            nb = nb + "0"
+        }
         
     }
     if(cpt.toString() === document.getElementById("Nombre a trouver").innerHTML){
-        console.log('o')
+        document.getElementById("gagne").classList.add("active")
+        document.getElementById("overlay").classList.add("show")
+    } else{
+        document.getElementById("perdu").classList.add("active")
+        document.getElementById("nombre").innerHTML = conv(parseInt(document.getElementById("Nombre a trouver").innerHTML))
+        document.getElementById("overlay").classList.add("show")
     }
-    console.log(cpt)
 }
